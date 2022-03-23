a14e5e09789752f91731f0462138e7153835e986
Abrar Faisal <47819765+abrarshovon@users.noreply.github.com>
2022-03-23 12:02:15 +0600

    jsonModel Version to view data from json file

--- showJson/views.py
+++ showJson/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import json
+def index(request):
+    f = open('./stock_market_data.json')
+    data = json.load(f)
+    si= len(data)
+    return render(request, 'showJson/index.html', {'data': data, 'si':si})
+
 
