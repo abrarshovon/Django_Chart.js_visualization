9ed8bca6ce23c3e6d5ec839b03002077aa36b97d
Abrar Faisal <47819765+abrarshovon@users.noreply.github.com>
2022-03-27 20:59:24 +0600

    sqlite model with visualization

--- showJson/views.py
+++ showJson/views.py
@@ -1,9 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import json
-
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
+from .models import JsonModel
 def index(request):
     f = open('./stock_market_data.json')
     data = json.load(f)
     si= len(data)
     return render(request, 'showJson/index.html', {'data': data, 'si':si})
+#def home(request):
+   # jsonmodel= JsonModel.objects.all()
+
+  #S  return render(request, 'showJson/main.html', {'data': jsonmodel})
+class JsonListView(ListView):
+    paginate_by = 20
+    model = JsonModel
+    template_name= 'showJson/main.html'
+    context_object_name= 'data'
+
+class JsonDetailView(DetailView):
+    model = JsonModel
+    template_name= 'showJson/jsondetail.html'
+    context_object_name= 'heda'
+class JsonCreateView(CreateView):
+    model = JsonModel
+    template_name= 'showJson/jsoncreate.html'
+    fields=['id', 'date','trade_code','high','low','open','close','volume']
+    success_url="/json/"
+class JsonUpdateView(UpdateView):
+    model = JsonModel
+    template_name= 'showJson/jsoncreate.html'
+    fields=['date','trade_code','high','low','open','close','volume']
+    success_url="/json/"
+class JsonDeleteView(DeleteView):
+    model = JsonModel
+    template_name= 'showJson/confirm.html'
+    success_url="/json/"
--- visualization/settings.py
+++ visualization/settings.py
@@ -9,13 +9,12 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
+BASE_DIR = BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -38,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'showJson',
+    'import_export',
     
 ]
-
+IMPORT_EXPORT_USE_TRANSACTIONS = True
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'visualization.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'visualization.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR , 'db.sqlite3'),
     }
 }
 
@@ -119,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR , 'static'),
 ]
 
 # Default primary key field type
